bd899e554338e0f512ed89d4c665d7f8f7b2b1ec
Aaran Lawing <lawingaaran@gmail.com>
2022-12-21 23:53:12 -0500

    NA

--- src/LoLScript.java
+++ src/LoLScript.java
@@ -1,5 +1,33 @@
-public class LoLScript {
-    public static void main(String[] args) throws Exception {
+import org.opencv.core.Core;
+import org.opencv.imgproc.Imgproc;
 
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+
+public class LoLScript {
+    static {
+        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+    }
+    public static void main(String[] args) throws Exception {
+        try {
+            String inFile = "champions/Ezreal_OriginalCircle.png";
+            String template = "src/screenshot.png";
+            String outFile = "cool.png";
+            Rectangle trackingArea = new Rectangle(1500, 650, 450, 450);
+            ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+            Robot robot = new Robot();
+
+            while (true) {
+                BufferedImage screenCapture = robot.createScreenCapture(trackingArea);
+                ImageIO.write(screenCapture, "png", new File("src/screenshot.png"));
+                new MatchTemplate().run(template, inFile, outFile, Imgproc.TM_CCOEFF);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+
+        }
     }
 }
